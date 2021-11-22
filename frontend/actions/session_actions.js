475f46a15362aa7790262113f9ae9faaee4c5c0a
wesley <cheung.w.2016@gmail.com>
2021-11-22 13:53:28 -0800

    added sessoin_actions.js

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,1 +1,44 @@
+import * as SessionApiUtil from "../util/session_api_util"
+
+//action constants
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER'
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
+// export const REMOVE_SESSION_ERRORS = 'REMOVE_SESSION_ERRORS'
+
+
+//action creators
+const receiveCurrentUser = (user) => ({
+    type: RECEIVE_CURRENT_USER,
+    user
+})
+
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER
+})
+
+const receiveSessionErrors = (errors) => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+})
+
+// const removeSessionErrors = () => ({
+//     type: REMOVE_SESSION_ERRORS
+// })
+
+export const login = (user) => dispatch => {
+    return SessionApiUtil.login(user)
+        .then(user => dispatch(receiveCurrentUser(user)),
+              errors => dispatch(receiveSessionErrors(errors)))
+}
+
+export const signup = (user) => dispatch => {
+    return SessionApiUtil.signup(user)
+        .then(user => dispatch(receiveCurrentUser(user)),
+              errors => dispatch(receiveSessionErrors(errors)))
+}
+
+export const logout = () => dispatch => {
+    return SessionApiUtil.logout()
+        .then(() => dispatch(logoutCurrentUser()))
+}
