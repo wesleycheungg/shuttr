5570837e55f1ec2d4fce48958d131b1c45143b1c
wesley <cheung.w.2016@gmail.com>
2021-11-25 01:36:55 -0800

    added more styling to user auth

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component{
     constructor(props){
@@ -10,7 +11,11 @@ class Home extends React.Component{
             <div className="homepage">
                 <div className="homepage-text">
                     <h1>Find your inspiration.</h1>
+                    <p>Join the Shuttr community, home to ten of billions of photos and 2 million groups.</p>
+                    <button className="start-for-free-btn"><Link className="free-btn" to="/signup">Start for Free</Link></button>
+
                 </div>
+
             </div>
         )
     }
