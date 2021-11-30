1260791a3117c4db660e13b487b88c5027c1e69b
wesley <cheung.w.2016@gmail.com>
2021-11-30 10:15:32 -0800

    added seed data for photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,19 +13,19 @@ User.create(username: "test", email: "test@gmail.com", full_name: "Test", passwo
 User.create(username: "demo", email: "demo@gmail.com", full_name: "Demo User", password: "password")
 
 p1 = Photo.create(user_id: 1, img_url: "m1.jpg")
-p1.photo.attach(io: File.open("/Users/wes/Desktop/shuttr/app/assets/images/mountain_landscape/m1.jpg"), filename: "m1.jpg")
+p1.photo.attach(io: open("https://shuttr-dev.s3.us-west-1.amazonaws.com/pBn26WM2vsG26FejC42KNDa7"), filename: "m1.jpg")
 
-p2 = Photo.create(user_id: 1, img_url: "m2.jpg")
-p2.photo.attach(io: File.open("/Users/wes/Desktop/shuttr/app/assets/images/mountain_landscape/m2.jpg"), filename: "m2.jpg")
+p2 = Photo.create(user_id: 3, img_url: "m2.jpg")
+p2.photo.attach(io: open("https://shuttr-dev.s3.us-west-1.amazonaws.com/4NYXommyaiQupoE7iFQi9EkU"), filename: "m2.jpg")
 
-p3 = Photo.create(user_id: 1, img_url: "m3.jpg")
-p3.photo.attach(io: File.open("/Users/wes/Desktop/shuttr/app/assets/images/mountain_landscape/m3.jpg"), filename: "m3.jpg")
+p3 = Photo.create(user_id: 3, img_url: "m3.jpg")
+p3.photo.attach(io: open("https://shuttr-dev.s3.us-west-1.amazonaws.com/T4ngmFXHCZTE8KN1JopyPSdx"), filename: "m3.jpg")
 
-p4 = Photo.create(user_id: 1, img_url: "m4.jpg")
-p4.photo.attach(io: File.open("/Users/wes/Desktop/shuttr/app/assets/images/mountain_landscape/m4.jpg"), filename: "m4.jpg")
+p4 = Photo.create(user_id: 3, img_url: "m4.jpg")
+p4.photo.attach(io: open("https://shuttr-dev.s3.us-west-1.amazonaws.com/T4ngmFXHCZTE8KN1JopyPSdx"), filename: "m4.jpg")
 
-p5 = Photo.create(user_id: 1, img_url: "m5.jpg")
-p5.photo.attach(io: File.open("/Users/wes/Desktop/shuttr/app/assets/images/mountain_landscape/m5.jpg"), filename: "m5.jpg")
+p5 = Photo.create(user_id: 3, img_url: "m5.jpg")
+p5.photo.attach(io: open("https://shuttr-dev.s3.us-west-1.amazonaws.com/6LTyhmWeyt5k1Gwh2Jo2f3o1"), filename: "m5.jpg")
 
 p6 = Photo.create(user_id: 1, img_url: "m6.jpg")
-p6.photo.attach(io: File.open("/Users/wes/Desktop/shuttr/app/assets/images/mountain_landscape/m6.jpg"), filename: "m6.jpg")
+p6.photo.attach(io: open("https://shuttr-dev.s3.us-west-1.amazonaws.com/2epnkeUcPMSKYE8RJw8WDiEG"), filename: "m6.jpg")
