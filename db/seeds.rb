3f361632069105921d66713d150fecf0ebe55de6
wesley <cheung.w.2016@gmail.com>
2021-11-30 14:59:12 -0800

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,15 +11,29 @@ User.destroy_all
 Photo.destroy_all
 
 user1 = User.create(username: "wcheung", email: "wcheung@gmail.com", full_name: "Wesley Cheung", password: "password")
-user2 = User.create(username: "wesleycheungg", email: "wesleycheung1998@gmail.com", full_name: "Wes Cheung", password: "password")
-user3 = User.create(username: "demo", email: "demo@gmail.com", full_name: "Demo User", password: "password")
+user2 = User.create(username: "demo", email: "demo@gmail.com", full_name: "Demo User", password: "password")
 
 p1 = Photo.new(user_id: user1.id, img_url: "alley.jpg")
 photo1 = URI.open("https://shuttr-seeds.s3.us-west-1.amazonaws.com/alley.jpg")
 p1.photo.attach(io: photo1, filename: "alley.jpg")
 p1.save
 
-# p2 = Photo.new(user_id: user3.id, img_url: "m1.jpg")
-# photo1 = URI.open("https://shuttr-pro.s3.us-west-1.amazonaws.com/DSC_0273.jpg")
-# p2.photo.attach(io: photo1, filename: "m1.jpg")
-# p2.save
+p2 = Photo.new(user_id: user2.id, img_url: "bamboo.jpg")
+photo2 = URI.open("https://shuttr-seeds.s3.us-west-1.amazonaws.com/bamboo.jpg")
+p2.photo.attach(io: photo2, filename: "bamboo.jpg")
+p2.save
+
+p3 = Photo.new(user_id: user2.id, img_url: "castle.jpg")
+photo3 = URI.open("https://shuttr-seeds.s3.us-west-1.amazonaws.com/castle.jpg")
+p3.photo.attach(io: photo3, filename: "castle.jpg")
+p3.save
+
+p4 = Photo.new(user_id: user2.id, img_url: "shrine.jpg")
+photo4 = URI.open("https://shuttr-seeds.s3.us-west-1.amazonaws.com/shrine.jpg")
+p4.photo.attach(io: photo4, filename: "shrine.jpg")
+p4.save
+
+p5 = Photo.new(user_id: user2.id, img_url: "kyoto.jpg")
+photo5 = URI.open("https://shuttr-seeds.s3.us-west-1.amazonaws.com/kyoto.jpg")
+p5.photo.attach(io: photo5, filename: "kyoto.jpg")
+p5.save
