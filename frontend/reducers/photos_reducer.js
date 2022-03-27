63571a329c1141a112ed3f3a2bc5026c475f7a20
wesley <cheung.w.2016@gmail.com>
2022-03-26 22:00:42 -0700

    first commit

--- frontend/reducers/photos_reducer.js
+++ frontend/reducers/photos_reducer.js
@@ -3,7 +3,7 @@ import { RECEIVE_ALL_PHOTOS, RECEIVE_PHOTO, REMOVE_PHOTO } from "../actions/phot
 
 const photoReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
-    let nextState = Object.assign({}, oldState);
+    let nextState = Object.assign({}, oldState); 
 
     switch (action.type) {
         case RECEIVE_ALL_PHOTOS:
